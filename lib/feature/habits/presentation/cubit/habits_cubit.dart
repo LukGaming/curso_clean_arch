ef30aaab6e4c23e94f85dea2471e4e4342ad7e24
Paulo <thelukgaming333@gmail.com>
2026-03-06 19:11:48 -0300

    Aula de Cubit

---
+++ lib/feature/habits/presentation/cubit/habits_cubit.dart
@@ -0,0 +1,65 @@
+import 'package:curso_clean_arch/feature/habits/domain/entities/habit.dart';
+import 'package:curso_clean_arch/feature/habits/domain/usecases/delete_habit_usecase.dart';
+import 'package:curso_clean_arch/feature/habits/domain/usecases/get_habits_usecase.dart';
+import 'package:curso_clean_arch/feature/habits/domain/usecases/insert_habit_usecase.dart';
+import 'package:curso_clean_arch/feature/habits/domain/usecases/update_habit_usecase.dart';
+import 'package:curso_clean_arch/feature/habits/presentation/cubit/habits_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:uuid/uuid.dart';
+
+class HabitsCubit extends Cubit<HabitsState> {
+  final GetHabitsUsecase _getHabitsUsecase;
+  final InsertHabitUseCase _insertHabitUseCase;
+  final UpdateHabitUsecase _updateHabitUsecase;
+  final DeleteHabitUsecase _deleteHabitUsecase;
+
+  HabitsCubit(
+    this._getHabitsUsecase,
+    this._insertHabitUseCase,
+    this._updateHabitUsecase,
+    this._deleteHabitUsecase,
+  ) : super(HabitsInitial());
+
+  Future<void> getHabits() async {
+    emit(HabitsLoading());
+
+    try {
+      final habits = await _getHabitsUsecase();
+      emit(HabitsLoaded(habits));
+    } catch (e) {
+      emit(HabitsError(e.toString()));
+    }
+  }
+
+  Future<void> insertHabit(String title) async {
+    try {
+      final habit = Habit(
+        id: Uuid().v4(),
+        title: title,
+        createdAt: DateTime.now(),
+      );
+      await _insertHabitUseCase(habit);
+      await getHabits();
+    } catch (e) {
+      emit(HabitsError(e.toString()));
+    }
+  }
+
+  Future<void> editHabit(Habit habit) async {
+    try {
+      await _updateHabitUsecase(habit);
+      await getHabits();
+    } catch (e) {
+      emit(HabitsError(e.toString()));
+    }
+  }
+
+  Future<void> deleteHabit(String id) async {
+    try {
+      await _deleteHabitUsecase(id);
+      await getHabits();
+    } catch (e) {
+      emit(HabitsError(e.toString()));
+    }
+  }
+}
